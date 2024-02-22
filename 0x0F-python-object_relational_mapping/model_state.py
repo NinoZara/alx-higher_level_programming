c2a374dd3b10d64d699bd0a841fd61264811c7e3
Alanwoke Martins <alanwokemartins@gmail.com>
2024-02-22 04:56:25 -0800

    3

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-""" Define State model """
+"""contains the class definition of a State and an instance
+Base = declarative_base() """
+
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer
+
 
 Base = declarative_base()
 
 
 class State(Base):
-        """ Define a class State to be linked to db table """
-            __tablename__ = 'states'
-
-                id = Column(Integer, nullable=False,
-                                        autoincrement=True, unique=True,
-                                                        primary_key=True)
-
-                    name = Column(String(128), nullable=False)
+        """ State Class inherit from Base declarative_base()
+            links to the MySQL table states
+                Attr:
+                        id, name
+                            """
+                                __tablename__ = 'states'
+                                    id = Column(Integer, autoincrement=True, primary_key=True)
+                                        name = Column(String(128), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-""" Define State model """
-from relationship_state import Base
+"""contains the class definition of a State and an instance
+Base = declarative_base() """
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, ForeignKey
+from relationship_state import Base, State
 
 
 class City(Base):
-        """ Define a class State to be linked to db table """
-            __tablename__ = 'cities'
-
-                id = Column(Integer, nullable=False,
-                                        autoincrement=True, unique=True,
-                                                        primary_key=True)
-
-                    name = Column(String(128), nullable=False)
-
-                        state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+        """ City Class inherit from Base declarative_base()
+            links to the MySQL table states
+                Attr:
+                        id, name
+                            """
+                                __tablename__ = 'cities'
+                                    id = Column(Integer, unique=True, autoincrement=True, primary_key=True)
+                                        name = Column(String(128), nullable=False)
+                                            state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
