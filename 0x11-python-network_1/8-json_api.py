4e32d846ae4c9015badc24bc531076e6dca644d3
Alanwoke Martins <alanwokemartins@gmail.com>
2024-03-05 16:04:22 -0800

    Json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""A script tha:
-- takes in a letter
-- sends POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter.
-"""
-import sys
-import requests
+"""A Python script that fetches
+https://alx-intranet.hbtn.io/status"""
 
+if __name__ == '__main__':
+    import requests
+    import sys
 
-if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
-
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    url = 'http://0.0.0.0:5000/search_user'
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
     try:
-        response = r.json()
-        if response == {}:
-            print("No result")
+        r = requests.post(url, data={'q': q})
+        json_obj = r.json()
+        if not bool(json_obj):
+            print('No result')
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print('[{}] {}'.format(json_obj['id'], json_obj['name']))
     except ValueError:
-        print("Not a valid JSON")
+        print('Not a valid JSON')
